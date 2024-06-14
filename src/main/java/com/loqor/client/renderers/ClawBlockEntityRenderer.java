9e4eb3aba82cc12d4756f907eb701207c082206e
Bug1312 <bug@bug1312.com>
2024-06-14 11:41:52 -0700

    finish functionality; visuals and audio required

--- src/main/java/com/loqor/client/renderers/ClawBlockEntityRenderer.java
+++ src/main/java/com/loqor/client/renderers/ClawBlockEntityRenderer.java
@@ -1,19 +1,31 @@
 package com.loqor.client.renderers;
 
 import com.loqor.core.blockentities.ClawBlockEntity;
-import com.loqor.core.blockentities.PunchGameBlockEntity;
 
-import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
 
 public class ClawBlockEntityRenderer<T extends ClawBlockEntity> implements BlockEntityRenderer<T> {
+	private static final float ITEM_SCALE = 0.7F;
+	private final ItemRenderer itemRenderer;
+
+	public ClawBlockEntityRenderer(BlockEntityRendererFactory.Context ctx) {
+		this.itemRenderer = ctx.getItemRenderer();
+	}
 
 	@Override
 	public void render(T be, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-	}
+		matrices.push();
+		matrices.translate(0.5F, 1.5F, 0.5F);
+		matrices.scale(ITEM_SCALE, ITEM_SCALE, ITEM_SCALE);
+		this.itemRenderer.renderItem(be.collectedItem, ModelTransformationMode.FIXED, light, overlay, matrices, vertexConsumers, be.getWorld(), (int) be.getPos().asLong());
+		matrices.pop();
 
+		// TODO: Animation
+	}
 
 }
--- src/main/java/com/loqor/core/blockentities/ClawBlockEntity.java
+++ src/main/java/com/loqor/core/blockentities/ClawBlockEntity.java
@@ -16,6 +16,7 @@ import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
 import net.minecraft.registry.RegistryWrapper.WrapperLookup;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -23,16 +24,16 @@ import net.minecraft.util.math.random.Random;
 import net.minecraft.world.tick.TickPriority;
 
 public class ClawBlockEntity extends TicketReturningBlockEntity.RequiresToken implements SidedInventory {
-    private final DefaultedList<ItemStack> items = DefaultedList.ofSize(27, ItemStack.EMPTY);
-    public ItemStack collectedItem = ItemStack.EMPTY;
-    public long collectedItemTick = 0;
-    
-    // 5s at normal tick rate
+	private final DefaultedList<ItemStack> items = DefaultedList.ofSize(27, ItemStack.EMPTY);
+	public ItemStack collectedItem = ItemStack.EMPTY;
+	public long collectedItemTick = 0;
+
+	// 5s at normal tick rate
 	public static final long WIN_DELAY = 20 * 5;
-	
+
 	private static final String NBT_KEY_COLLECTED_ITEM = "CollectedItem";
 	private static final String NBT_KEY_COLLECTED_ITEM_TICK = "CollectedItemTick"; 
-	
+
 	public ClawBlockEntity(BlockPos pos, BlockState state) {
 		super(VJBlockEntityTypes.CLAW, pos, state);
 	}
@@ -40,72 +41,79 @@ public class ClawBlockEntity extends TicketReturningBlockEntity.RequiresToken im
 	@Override
 	public void activate() {
 		super.activate();
-		
-		this.collectedItem = this.removeRandomItem();
-		this.collectedItemTick = world.getTime();
 
-		world.scheduleBlockTick(pos, getCachedState().getBlock(), (int) PunchGameBlockEntity.WIN_DELAY, TickPriority.NORMAL);
+		// TODO: Play sound
+
+		if (!this.world.isClient()) {
+			this.collectedItem = this.removeRandomItem();
+			this.collectedItemTick = world.getTime();
+
+			world.scheduleBlockTick(pos, getCachedState().getBlock(), (int) PunchGameBlockEntity.WIN_DELAY, TickPriority.NORMAL);
+
+			((ServerWorld) this.world).getChunkManager().markForUpdate(this.pos);
+		}
 	}
-	
+
 	@Override
 	public void deactivate() {
 		super.deactivate();
 
 		this.collectedItem = ItemStack.EMPTY;
 		this.collectedItemTick = 0;
+
+		if (!this.world.isClient()) ((ServerWorld) this.world).getChunkManager().markForUpdate(this.pos);
 	}
-	
+
 	private ItemStack removeRandomItem() {
 		if (!this.isEmpty()) {
 			final Random random = this.world.random;
 			if (random.nextFloat() <= 0.6f) {
 				final List<ItemStack> stacks = this.items.stream().filter(s -> !s.isEmpty()).collect(Collectors.toList());
-				if (stacks.size() > 0) {
+				if (!stacks.isEmpty()) {
 					final ItemStack stack = stacks.get(random.nextInt(stacks.size()));
 					final int slot = this.items.indexOf(stack);
 					if (slot != -1) return this.removeStack(slot, 1);
 				}
 			}
 		}
-		
+
 		return ItemStack.EMPTY;
 	}
-	
+
 	@Override
 	protected void writeNbt(NbtCompound nbt, WrapperLookup registryLookup) {
 		super.writeNbt(nbt, registryLookup);
-        
+
 		Inventories.writeNbt(nbt, this.items, true, registryLookup);
-        
-		if (!this.collectedItem.isEmpty())
-        	nbt.put(NBT_KEY_COLLECTED_ITEM, this.collectedItem.encode(registryLookup));
-        
+
+		if (!this.collectedItem.isEmpty()) nbt.put(NBT_KEY_COLLECTED_ITEM, this.collectedItem.encode(registryLookup));
+
 		nbt.putLong(NBT_KEY_COLLECTED_ITEM_TICK, this.collectedItemTick);
 	}
-	
+
 	@Override
 	protected void readNbt(NbtCompound nbt, WrapperLookup registryLookup) {
 		super.readNbt(nbt, registryLookup);
-        
-		this.items.clear();
-        Inventories.readNbt(nbt, this.items, registryLookup);
 
-        if (nbt.contains(NBT_KEY_COLLECTED_ITEM, NbtElement.COMPOUND_TYPE))
-        	this.collectedItem = ItemStack.fromNbt(registryLookup, nbt.get(NBT_KEY_COLLECTED_ITEM)).orElse(ItemStack.EMPTY);
-        else 
-        	this.collectedItem = ItemStack.EMPTY;
-        
-        this.collectedItemTick = nbt.getLong(NBT_KEY_COLLECTED_ITEM_TICK);
+		this.items.clear();
+		Inventories.readNbt(nbt, this.items, registryLookup);
+
+		if (nbt.contains(NBT_KEY_COLLECTED_ITEM, NbtElement.COMPOUND_TYPE))
+			this.collectedItem = ItemStack.fromNbt(registryLookup, nbt.get(NBT_KEY_COLLECTED_ITEM)).orElse(ItemStack.EMPTY);
+		else 
+			this.collectedItem = ItemStack.EMPTY;
+
+		this.collectedItemTick = nbt.getLong(NBT_KEY_COLLECTED_ITEM_TICK);
 	}
-	
+
 	@Override
 	public Packet<ClientPlayPacketListener> toUpdatePacket() {
 		return BlockEntityUpdateS2CPacket.create(this);
 	}
-	
+
 	@Override
 	public NbtCompound toInitialChunkDataNbt(WrapperLookup registryLookup) {
-	    return createNbt(registryLookup);
+		return createNbt(registryLookup);
 	}
 
 	@Override
@@ -115,7 +123,7 @@ public class ClawBlockEntity extends TicketReturningBlockEntity.RequiresToken im
 
 	@Override
 	public boolean isEmpty() {
-        return this.items.isEmpty();
+		return this.items.isEmpty();
 	}
 
 	@Override
@@ -125,9 +133,9 @@ public class ClawBlockEntity extends TicketReturningBlockEntity.RequiresToken im
 
 	@Override
 	public ItemStack removeStack(int slot, int amount) {
-        ItemStack itemStack = Inventories.splitStack(this.items, slot, amount);
-        if (!itemStack.isEmpty()) this.markDirty();
-        return itemStack;
+		ItemStack itemStack = Inventories.splitStack(this.items, slot, amount);
+		if (!itemStack.isEmpty()) this.markDirty();
+		return itemStack;
 	}
 
 	@Override
@@ -137,9 +145,9 @@ public class ClawBlockEntity extends TicketReturningBlockEntity.RequiresToken im
 
 	@Override
 	public void setStack(int slot, ItemStack stack) {
-        this.items.set(slot, stack);
-        stack.capCount(this.getMaxCount(stack));
-        this.markDirty();
+		this.items.set(slot, stack);
+		stack.capCount(this.getMaxCount(stack));
+		this.markDirty();
 	}
 
 	@Override
@@ -152,15 +160,15 @@ public class ClawBlockEntity extends TicketReturningBlockEntity.RequiresToken im
 		this.items.clear();
 	}
 
-    private static final int[] ALL_SLOTS;
-    static {
-        int[] array = new int[27];
-        for (int i = 0; i <= 26; i++) {
-            array[i] = i;
-        }
-        ALL_SLOTS = array; // I ain' typing allat
-    }
-    
+	private static final int[] ALL_SLOTS;
+	static {
+		int[] array = new int[27];
+		for (int i = 0; i <= 26; i++) {
+			array[i] = i;
+		}
+		ALL_SLOTS = array; // I ain' typing allat
+	}
+
 	@Override
 	public int[] getAvailableSlots(Direction var1) {
 		return ALL_SLOTS;
